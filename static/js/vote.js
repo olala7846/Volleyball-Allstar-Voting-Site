276fa8e024dd8a49a75187ab9f5797433085a82d
hsincheng Chao <olala7846@gmail.com>
2016-03-01 22:55:18 +0800

    more UI on voting page

--- static/js/vote.js
+++ static/js/vote.js
@@ -1,22 +1,29 @@
+/* global _ */
 // vote.js
 angular.module('voteApp', [])
   .controller('VoteController', ['$scope', '$window', function($scope, $window){
     $scope.election = $window.data;
+    var positions = $scope.election.positions;
 
     $scope.validateVotes = function(){
-      for(var i in $scope.election.positions){
-        var position = $scope.election.positions[i];
+      for(var i in positions){
+        var position = positions[i];
         var votesPerPerson = position.votes_per_person;
-        var numSelected = 0;
-        for(var j in position.candidates){
-          var candidate = position.candidates[j];
-          if(candidate.selected){
-            numSelected++;
-          }
-        }
-        position.valid = numSelected <= votesPerPerson;
+        var candidates = position.candidates;
+        var results = _.countBy(candidates, function(candidate){
+          return candidate.selected? 'selected':'notSelected';
+        });
+        position.voted = results.selected || 0;
+        position.valid = position.voted <= votesPerPerson;
       }
     };
+
+    function updateCandidate(candidate){
+      if(!candidate.selected){
+        candidate.selected = false;
+      }
+      candidate.btnTitle = candidate.selected? '收回選票':'投他一票';
+    }
 
     $scope.toggleCandidate = function(candidate) {
       if(!candidate.selected){
@@ -24,9 +31,35 @@ angular.module('voteApp', [])
       } else {
         candidate.selected = false;
       }
+      updateCandidate(candidate);
       $scope.validateVotes();
     };
 
+    $scope.candidateClass = function(candidate){
+      var candidatePosition = _.find($scope.election.positions, function(position){
+        return _.indexOf(position.candidates, candidate) != -1;
+      });
+      if(candidatePosition.valid) {
+        if(candidate.selected){
+          return 'card-inverse card-success';
+        } else {
+          return '';
+        }
+      } else {
+        return 'card-inverse card-danger';
+      }
+    };
+
+    function initCandidates(){
+      for(var i in positions){
+        var candidates = positions[i].candidates;
+        for(var j in candidates){
+          updateCandidate(candidates[j]);
+        }
+      }
+    }
+
+    initCandidates();
     $scope.validateVotes();  // initial validation
 
   }]);

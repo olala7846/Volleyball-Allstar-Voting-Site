e8d4ce602b0105305496503f93d02309b7a6cc41
hsincheng Chao <olala7846@gmail.com>
2016-03-07 20:39:02 +0800

    change schoolId to studentId

--- static/js/register.js
+++ static/js/register.js
@@ -2,20 +2,20 @@
 angular.module('registerApp', [])
   .controller('RegisterFormController', ['$scope', '$http', '$window',
     function($scope, $http, $window) {
-      $scope.schoolId = '';
+      $scope.studentId = '';
       $scope.electionKey = $window.election_key;
       $scope.valid = false;
       $scope.inputGroupClass = 'has-warning';
       $scope.validate = function() {
-        var schoolId = $scope.schoolId;
+        var studentId = $scope.studentId;
         var validPattern = new RegExp('[a-zA-z][0-9]{8}$');
-        var match = schoolId.match(validPattern);
+        var match = studentId.match(validPattern);
         $scope.valid = false;
         $scope.inputGroupClass = 'has-danger';
-        if(match != null && match[0] == schoolId) {
+        if(match != null && match[0] == studentId) {
           $scope.valid = true;
           $scope.inputGroupClass = 'has-success';
-        } else if ($scope.schoolId === '') {
+        } else if ($scope.studentId === '') {
           $scope.inputGroupClass = 'has-warning';
         }
 
@@ -24,7 +24,7 @@ angular.module('registerApp', [])
       $scope.requestEmail = function(){
         var apiPath = '/api/send_voting_email';
         var param = {
-          student_id: $scope.schoolId,
+          student_id: $scope.studentId,
           election_key: $scope.electionKey
         };
         $http.post(apiPath, param).then(function(response){

5b15a1c5e577cb966d2eaba943f148c39397c557
hsincheng Chao <olala7846@gmail.com>
2016-03-29 12:58:31 +0800

    support teacher ntu email

--- static/js/register.js
+++ static/js/register.js
@@ -9,8 +9,9 @@ angular.module('registerApp', [])
 
       $scope.validate = function() {
         var studentId = $scope.studentId;
-        var validPattern = new RegExp('[a-zA-Z][0-9]{2}[a-zA-Z0-9][0-9]{5}$');
-        var match = studentId.match(validPattern);
+        var studentPattern = new RegExp('[a-zA-Z][0-9]{2}[a-zA-Z0-9][0-9]{5}$');
+        var teacherPattern = new RegExp('[a-zA-Z]+$');
+        var match = studentId.match(studentPattern) || studentId.match(teacherPattern);
         $scope.valid = false;
         $scope.inputGroupClass = 'has-danger';
         if(match != null && match[0] == studentId) {
